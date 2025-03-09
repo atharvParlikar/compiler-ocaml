9b9461adf5910d14d92a89b247d830c210b0eb2b
atharv parlikar <atharvparlikar@gmail.com>
2025-03-09 19:45:48 +0530

    Parser complete

--- bin/main.ml
+++ bin/main.ml
@@ -1,38 +1,69 @@
-type token =
-  | LET                        (* "let" keyword *)
-  | IF                         (* "if" keyword *)
-  | THEN                       (* "then" keyword *)
-  | ELSE                       (* "else" keyword *)
-  | IDENTIFIER of string       (* Variable names, function names, etc. *)
-  | INT of int                 (* Integer literals *)
-  | FLOAT of float             (* Float literals *)
-  | STRING of string           (* String literals *)
-  | EQUALS                     (* "=" symbol *)
-  | EQUALITY                   (* "==" symbol *)
-  | PLUS                       (* "+" operator *)
-  | MINUS                      (* "-" operator *)
-  | STAR                       (* "*" operator *)
-  | SLASH                      (* "/" operator *)
-  | LPAREN                     (* "(" symbol *)
-  | RPAREN                     (* ")" symbol *)
-  | LBRACE                     (* "{" symbol *)
-  | RBRACE                     (* "}" symbol *)
-  | SEMICOLON                  (* ";" symbol *)
-  | EOF                        (* End of file *)
-;;
 exception SyntaxError of string
+
+type token =
+  | Let                        (* "let" keyword *)
+  | If                         (* "if" keyword *)
+  | Then                       (* "then" keyword *)
+  | Else                       (* "else" keyword *)
+  | True
+  | False
+  | Identifier of string       (* Variable names, function names, etc. *)
+  | Int of int                 (* Integer literals *)
+  | Float of float             (* Float literals *)
+  | String of string           (* String literals *)
+  | Equals                     (* "=" symbol *)
+  | Equality                   (* "==" symbol *)
+  | NotEquality
+  | Less
+  | LessEqual
+  | Greater
+  | GreaterEqual
+  | Plus                       (* "+" operator *)
+  | Minus                      (* "-" operator *)
+  | Star                       (* "*" operator *)
+  | Slash                      (* "/" operator *)
+  | Lparen                     (* "(" symbol *)
+  | Rparen                     (* ")" symbol *)
+  | Lbrace                     (* "{" symbol *)
+  | Rbrace                     (* "}" symbol *)
+  | Semicolon                  (* ";" symbol *)
+  | Bang                       (* "!" symbol *)
+  | Nil                        (* nil keyword *)
+  | Eof                        (* End of file *)
+;;
+
+(*type literal =*)
+(*  | Number*)
+(*  | String*)
+(*  | True*)
+(*  | False*)
+(*  | Nil*)
+(*;;*)
+(**)
+(*type operator =*)
+(*  | Equal*)
+(*  | NotEqual*)
+(*  | Less*)
+(*  | LessEqual*)
+(*  | Greater*)
+(*  | GreaterEqual*)
+(*  | Add*)
+(*  | Subtract*)
+(*  | Multiply*)
+(*  | Divide*)
+(*;;*)
 
 let tokenize input =
   let len = String.length input in
   let rec aux pos tokens =
-    if pos >= len then List.rev (EOF :: tokens)
+    if pos >= len then List.rev (Eof :: tokens)
     else
       match input.[pos] with
       | ' ' | '\n' | '\t' -> aux (pos + 1) tokens
-      | '+' -> aux (pos + 1) (PLUS :: tokens)
-      | '-' -> aux (pos + 1) (MINUS :: tokens)
-      | '*' -> aux (pos + 1) (STAR :: tokens)
-      | '/' -> aux (pos + 1) (SLASH :: tokens)
+      | '+' -> aux (pos + 1) (Plus :: tokens)
+      | '-' -> aux (pos + 1) (Minus :: tokens)
+      | '*' -> aux (pos + 1) (Star :: tokens)
+      | '/' -> aux (pos + 1) (Slash :: tokens)
       | '=' -> let start = pos in
                let rec find_end end_pos =
                   if end_pos < len && input.[end_pos] = '=' then find_end (end_pos + 1)
@@ -41,15 +72,24 @@ let tokenize input =
                let end_pos = find_end start in
                let token = String.sub input start (end_pos - start) in
                (match token with
-                | "==" -> aux (end_pos) (EQUALITY :: tokens)
-                | "=" -> aux (start + 1) (EQUALS :: tokens)
+                | "==" -> aux (end_pos) (Equality :: tokens)
+                | "=" -> aux (start + 1) (Equals :: tokens)
                 | _ -> raise (SyntaxError (Printf.sprintf "Invalid token in position %d, token: %s" start token))
                )
-      | '(' -> aux (pos + 1) (LPAREN :: tokens)
-      | ')' -> aux (pos + 1) (RPAREN :: tokens)
-      | '{' -> aux (pos + 1) (LBRACE :: tokens)
-      | '}' -> aux (pos + 1) (RBRACE :: tokens)
-      | ';' -> aux (pos + 1) (SEMICOLON :: tokens)
+      | '!' -> if pos + 1 < len && input.[pos + 1] = '='
+               then aux (pos + 2) (NotEquality :: tokens)
+               else aux (pos + 1) (Bang :: tokens)
+      | '<' -> if pos + 1 < len && input.[pos + 1] = '='
+              then aux (pos + 2) (LessEqual :: tokens)
+              else aux (pos + 1) (Less :: tokens)
+      | '>' -> if pos + 1 < len && input.[pos + 1] = '='
+              then aux (pos + 2) (GreaterEqual :: tokens)
+              else aux (pos + 1) (Greater :: tokens)
+      | '(' -> aux (pos + 1) (Lparen :: tokens)
+      | ')' -> aux (pos + 1) (Rparen :: tokens)
+      | '{' -> aux (pos + 1) (Lbrace :: tokens)
+      | '}' -> aux (pos + 1) (Rbrace :: tokens)
+      | ';' -> aux (pos + 1) (Semicolon :: tokens)
       | '"' ->
           let start = pos in
           let rec find_end pos =
@@ -60,7 +100,7 @@ let tokenize input =
           in
           let end_pos = find_end pos in
           let str_contents = String.sub input (start + 1) (end_pos - start - 1) in
-          aux (end_pos + 1) (STRING str_contents :: tokens)
+          aux (end_pos + 1) (String str_contents :: tokens)
 
       | '0' .. '9' ->
           let start = pos in
@@ -74,26 +114,33 @@ let tokenize input =
           in
           let (end_pos, is_float) = find_end pos false in
           let num_string = String.sub input start (end_pos - start) in
-          if is_float then aux end_pos (FLOAT (float_of_string num_string) :: tokens)
-          else aux end_pos (INT (int_of_string num_string) :: tokens)
-        
+          if is_float then aux end_pos (Float (float_of_string num_string) :: tokens)
+          else aux end_pos (Int (int_of_string num_string) :: tokens)
+
       | 'a' .. 'z' | 'A' .. 'Z' ->
         let start = pos in
         let rec find_end end_pos =
-          if end_pos < len && (
-            (65 <= (Char.code input.[end_pos]) && (Char.code input.[end_pos]) <= 90)
-            || (97 <= (Char.code input.[end_pos]) && (Char.code input.[end_pos]) <= 122)
-          ) then find_end (end_pos + 1)
-          else (end_pos)
+          if end_pos < len then
+            let c = Char.code input.[end_pos] in
+            if (65 <= c && c <= 90) ||    (* A-Z *)
+               (97 <= c && c <= 122) ||   (* a-z *)
+               (48 <= c && c <= 57) ||    (* 0-9 *)
+               c = 95                    (* _ *)
+            then find_end (end_pos + 1)
+            else end_pos
+          else end_pos
         in
         let end_pos = find_end start in
         let token = String.sub input start (end_pos - start) in
         (match token with
-          | "if" -> aux end_pos (IF :: tokens)
-          | "else" -> aux end_pos (ELSE :: tokens)
-          | "then" -> aux end_pos (THEN :: tokens)
-          | "let" -> aux end_pos (LET :: tokens)
-          | _ -> aux end_pos (IDENTIFIER token:: tokens)
+          | "if" -> aux end_pos (If :: tokens)
+          | "else" -> aux end_pos (Else :: tokens)
+          | "then" -> aux end_pos (Then :: tokens)
+          | "let" -> aux end_pos (Let :: tokens)
+          | "true" -> aux end_pos (True :: tokens)
+          | "false" -> aux end_pos (False :: tokens)
+          | "nil" -> aux end_pos (Nil :: tokens)
+          | _ -> aux end_pos (Identifier token :: tokens)
         )
       | _ -> aux (pos + 1) tokens
     in aux 0 [];;
@@ -103,30 +150,33 @@ let rec print_token_list tokens =
   | [] -> print_endline ""
   | token :: rest ->
       (match token with
-      | INT n -> print_string ("INT(" ^ string_of_int n ^ ")")
-      | PLUS -> print_string "+"
-      | MINUS -> print_string "-"
-      | STAR -> print_string "*"
-      | SLASH -> print_string "/"
-      | EOF -> print_string ""
-      | EQUALS -> print_string "="
-      | EQUALITY -> print_string "=="
-      | IF -> print_string "IF"
-      | ELSE -> print_string "ELSE"
-      | THEN -> print_string "THEN"
-      | LET -> print_string "LET"
-      | LPAREN -> print_string "("
-      | RPAREN -> print_string ")"
-      | LBRACE -> print_string "{"
-      | RBRACE -> print_string "}"
-      | STRING string -> print_string ("\"" ^ string ^ "\"")
-      | SEMICOLON -> print_string("SEMICOLON")
-      | FLOAT float -> print_string("FLOAT(" ^ string_of_float float ^ ")")
-      | IDENTIFIER string -> print_string ("IDENTIFIER(" ^ string ^ ")"));
+      | Int n -> print_string ("INT(" ^ string_of_int n ^ ")")
+      | Plus -> print_string "+"
+      | Minus -> print_string "-"
+      | Star -> print_string "*"
+      | Slash -> print_string "/"
+      | Eof -> print_string ""
+      | Equals -> print_string "="
+      | Equality -> print_string "=="
+      | If -> print_string "IF"
+      | Else -> print_string "ELSE"
+      | Then -> print_string "THEN"
+      | Let -> print_string "LET"
+      | Lparen -> print_string "("
+      | Rparen -> print_string ")"
+      | Lbrace -> print_string "{"
+      | Rbrace -> print_string "}"
+      | String string -> print_string ("\"" ^ string ^ "\"")
+      | Semicolon -> print_string("SEMICOLON")
+      | Float float -> print_string("FLOAT(" ^ string_of_float float ^ ")")
+      | Identifier string -> print_string ("IDENTIFIER(" ^ string ^ ")")
+      | Nil -> print_string "NIL"
+      | _ -> print_string "wait lil bro"
+    );
       print_string " ";
       print_token_list rest;;
 
-let syntax_string = {|let pi = 3.1415;|};;
+let syntax_string = {|let  = "atharv";|};;
 let tokens = tokenize syntax_string;;
 
 print_token_list tokens;;
